8a397ff99929106f70afb60da122da0f154e8462
Celi0905 <celestinsaison@gmail.com>
2026-02-20 15:22:23 +0100

    Delete Celihub_devloader.lua

--- Celihub_devloader.lua
+++
@@ -1,4 +0,0 @@
-CeliHub DEV LOADER | Kein Key benötigt!
--- Nur für Early Access Tester
--- Made by Celi 💫
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Celi0905/celihub/main/CeliHub_Dev.lua", true))()
